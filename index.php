d6bc459c1bc627b4d6876850911fd427a3e6ed88
Kim Wook <kquan821@gmail.com>
2022-12-17 01:05:42 +0700

    as

--- index.php
+++ index.php
@@ -49,35 +49,6 @@
     <div class="container">
         <p style="font-size: 25px;font-weight: bold;font-family: 'Times New Roman', Times, serif;color: #c05353;margin-bottom: 20px;">GỢI Ý SẢN PHẨM</p>
     </div>
-    <?php include('carouselop.php') ?>
-    <div class="container" style="margin-top: 30px;">
-        <div class="row">
-            <div class="col-sm-4"><a href="product_1.php"><img style="border-radius: 5px;" src="./images/design_sua/img_product_banner_1.jpg" alt=""></a></div>
-            <div class="col-sm-4"><a href="product_2.php"><img style="border-radius: 5px;" src="./images/design_sua/img_product_banner_2.jpg" alt=""></a></div>
-            <div class="col-sm-4"><a href="product_1.php"><img style="border-radius: 5px;" src="./images/design_sua/img_product_banner_3.jpg" alt=""></a></div>
-        </div>
-    </div>
-    <div class="container" style="margin-top: 40px;">
-        <p style="font-size: 25px;font-weight: bold;font-family: 'Times New Roman', Times, serif;color: #c05353;margin-bottom: 20px;">DANH MỤC HOT</p>
-    </div>
-    <div class="container" style="margin-top: 30px;">
-        <div class="row" style="margin-bottom: 20px;">
-            <div class="col-sm-2"><a style="display: flex;flex-direction: column;" href=""><span><img style="border-radius: 5px;" src="./images/design_sua/sua_bot_1.jpg" alt=""></span><span style="padding-left: 25px;">Son Môi</span></a></div>
-            <div class="col-sm-2"><a style="display: flex;flex-direction: column;" href=""><span><img style="border-radius: 5px;" src="./images/design_sua/sua_bot_2.jpg" alt=""></span></a><span style="padding-left: 25px;">Tinh Chất</span></div>
-            <div class="col-sm-2"><a style="display: flex;flex-direction: column;" href=""><span><img style="border-radius: 5px;" src="./images/design_sua/sua_chua_3.jpg" alt=""></span></a><span style="padding-left: 35px;">Mặt Nạ</span></div>
-            <div class="col-sm-2"><a style="display: flex;flex-direction: column;" href=""><span><img style="border-radius: 5px;" src="./images/design_sua/sua_tuoi_4.jpg" alt=""></span></a><span style="padding-left: 19px;">Kem Dưỡng</span></div>
-            <div class="col-sm-2"><a style="display: flex;flex-direction: column;" href=""><span><img style="border-radius: 5px;" src="./images/design_sua/sua_cao_5.jpg" alt=""></span></a><span style="padding-left: 17px;">Sữa Rửa Mặt</span></div>
-            <div class="col-sm-2"><a style="display: flex;flex-direction: column;" href=""><span><img style="border-radius: 5px;" src="./images/design_sua/sua_me_6.jpg" alt=""></span></a><span style="padding-left: 4x;">Nước Hoa Hồng</span></div>
-        </div>
-        <div class="row">
-            <div class="col-sm-2"><a style="display: flex;flex-direction: column;" href=""><span><img style="border-radius: 5px;" src="./images/design_sua/sua_bot_3.jpg" alt=""></span></a><span style="padding-left: 25px;">Mini Size</span></div>
-            <div class="col-sm-2"><a style="display: flex;flex-direction: column;" href=""><span><img style="border-radius: 5px;" src="./images/design_sua/sua_chua_1.jpg" alt=""></span></a><span style="padding-left: 18px;">Tẩy Da Chết</span></div>
-            <div class="col-sm-2"><a style="display: flex;flex-direction: column;" href=""><span><img style="border-radius: 5px;" src="./images/design_sua/sua_tuoi_2.jpg" alt=""></span></a><span style="padding-left: 1px;">Kem Chống Nắng</span></div>
-            <div class="col-sm-2"><a style="display: flex;flex-direction: column;" href=""><span><img style="border-radius: 5px;" src="./images/design_sua/sua_me_6.jpg" alt=""></span></a><span style="padding-left: 21px;">Dưỡng Thể</span></div>
-            <div class="col-sm-2"><a style="display: flex;flex-direction: column;" href=""><span><img style="border-radius: 5px;" src="./images/design_sua/sua_cao_1.jpg" alt=""></span></a><span style="padding-left: 25px;">Tẩy Trang</span></div>
-            <div class="col-sm-2"><a style="display: flex;flex-direction: column;" href=""><span><img style="border-radius: 5px;" src="./images/design_sua/sua_me_1.jpg" alt=""></span></a><span style="padding-left: 37px;">Kẻ Mắt</span></div>
-        </div>
-    </div>
     <!-- Start Shop Services Area -->
     <section class="shop-services section home" style="margin-top: 30px;">
         <div class="container">
@@ -121,27 +92,6 @@
             </div>
         </div>
     </section>
-    <!-- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span class="ti-close" aria-hidden="true"></span></button>
-                </div>
-                <div class="modal-body">
-                    <div class="row no-gutters">
-                        <div class="col-lg-6 offset-lg-3 col-12">
-                            <h4 style="margin-top:100px;font-size:14px; font-weight:500; color:#e28585; display:block; margin-bottom:5px;">Eshop Free Lite</h4>
-                            <h3 style="font-size:30px;color:#333;">Currently You are using free lite Version of Eshop.<h3>
-                                    <p style="display:block; margin-top:20px; color:#888; font-size:14px; font-weight:400;">Please, purchase full version of the template to get all pages, features and commercial license</p>
-                                    <div class="button" style="margin-top:30px;">
-                                        <a href="https://wpthemesgrid.com/downloads/eshop-ecommerce-html5-template/" target="_blank" class="btn" style="color:#fff;">Buy Now!</a>
-                                    </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
     <?php
     include 'component/footer.php'
     ?>
